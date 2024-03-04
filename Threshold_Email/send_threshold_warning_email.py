6de59afe05dca59408a8143a3641389d761cd3c0
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2024-03-04 15:55:25 -0500

    format data read and email sending

--- Threshold_Email/send_threshold_warning_email.py
+++ Threshold_Email/send_threshold_warning_email.py
@@ -1,17 +1,16 @@
 #!/proj/sot/ska3/flight/bin/python
 
-import sys
 import os
-import datetime
-import Chandra.Time
 import argparse
 import getpass
 import traceback
+import json
+import urllib.request
+from astropy.table import Table
 
-#Define directory Pathing
-
-ACE_DATA_DIR = "/data/mta4/Space_Weather/ACE/Data"
-GOES_DATA_DIR = "/data/mta4/Space_Weather/GOES/Data"
+#Define data source pathing
+ACE_DATA_SOURCE = 'https://services.swpc.noaa.gov/json/ace/epam/ace_epam_5m.json'
+GOES_DATA_SOURCE = 'https://services.swpc.noaa.gov/json/goes/primary/differential-protons-6-hour.json'
 
 #TODO which email group to send for ACE and which to send for HRC Prox using GOES?
 ACE_ADMIN = ['sot_ace_alert@cfa.harvard.edu']
@@ -29,23 +28,126 @@ HRC_ADMIN = ['william.aaron@cfa.harvard.edu']
 
 #In use for old generation GOES-1 to GOES-15
 #DOES NOT MAP DIRECTLY TO NEW GOES CHANNELS
-OLD_GEN_HRC_PROXY = {'P4': 6000,
-                     'P5': 270000,
-                     'P6': 100000,
+OLD_GEN_HRC_PROXY = {'CHANNELS': {'P4': 6000,
+                                  'P5': 270000,
+                                  'P6': 100000},
                      'CONSTANT': 0}
 
 #In use for transition period between GOES-15 and GOES-16+ (2020)
-HRC_PROXY_V1 = {'P5': 6000,
-                'P7': 270000,
-                'P8A': 100000,
+HRC_PROXY_V1 = {'CHANNELS': {'P5': 6000,
+                             'P7': 270000,
+                             'P8A': 100000},
                 'CONSTANT': 0}
 
 #In use for new generation GOES-16+ (post 2020)
-HRC_PROXY_V2 = {'P5': 143,
-                'P6': 64738,
-                'P7': 162505,
+HRC_PROXY_V2 = {'CHANNELS': {'P5': 143,
+                             'P6': 64738,
+                             'P7': 162505}, 
                 'CONSTANT': 4127}
+#
+#--- Determine thresholds for ACE and HRC proxy violations
+#
 
+#TODO - Move these thresholds into the future MTA limits combined SQLite database
+
+#Based on Fluence calculated over time
+ACE_THRESHOLD = {'Violation': 3.6e8}
+
+#Based on HRC Proxy value as specific time
+HRC_THRESHOLD = {'Warning': 6.2e4,
+                 'Violation': 1.95e5}
+
+def check_data_for_viol():
+    ace_table = extract_data_table(ACE_DATA_SOURCE)
+    goes_table = extract_data_table(GOES_DATA_SOURCE)
+
+    ace_violation(ace_table)
+    hrc_violation(goes_table)
+
+
+def extract_data_table(jlink):
+    """
+    extract satellite data
+    input: jlink--- JSON web address or file
+    output: table--- astropy table of the data.
+    """
+#
+#--- read json file from a file or the web
+#
+    if os.path.isfile(jlink):
+        try:
+            with open(jlink) as f:
+                data = json.load(f)
+        except:
+            traceback.print_exc()
+            data = []
+    else:
+        try:
+            with urllib.request.urlopen(jlink) as url:
+                data = json.loads(url.read().decode())
+        except:
+            traceback.print_exc()
+            data = []
+
+    if len(data) < 1:
+        exit(1)
+
+    data = Table(data)
+    return data
+
+def ace_violation(table):
+    """
+    input: table --- astropy table of ace data in reverse time order
+    output: none, but sends a warning email to ACE admin if the P3 fluence is in violation.
+    """
+#
+#--- Average over the past 2 hour timeframe.
+#
+    p130a = table['P3'][:24].data.mean()
+    # Compute fluence over two hours from the averaged rate
+    p130f = p130a * 7200
+
+    if p130f > ACE_THRESHOLD['Warning']:
+        content = f"WARNING: A Radiation violation of P3 (130Kev) has been observed by ACE\n"
+        content += f"Observed: {p130f} \n"
+        content += f"Limit: {ACE_THRESHOLD['Warning']} particles/cm2-ster-MeV within 2 hours.\n"
+        content += f"see http://cxc.harvard.edu/mta/ace.html or https://cxc.harvard.edu/mta/RADIATION_new/ACE/ace.html\n"
+        content += f"The ACIS on-call person should review the data and call a telecon if necessary.\n"
+
+        subject = f"[sot_ace_alert] ACE_p3"
+        send_mail(content, subject, ACE_ADMIN)
+        
+def hrc_violation(table):
+    """
+    input: table --- astropy table of GOES data
+    output: none, but send a warning email to HRC admin if the HRC proxy is in violation
+    """
+
+    #TODO Send email for multiple proxies? Current just run for v2
+    calc_proxy = 0
+    for channel in HRC_PROXY_V2['CHANNELS'].keys():
+        calc_proxy += HRC_PROXY_V2['CHANNELS'][channel] * table[channel]
+    calc_proxy += HRC_PROXY_V2['CONSTANT']
+    
+    content = ''
+    subject = ''
+    if calc_proxy > HRC_THRESHOLD['Warning']:
+        content = f"WARNING: A HRC GOES proxy violation has been observerd by GOES\n"
+        content += f"Observed: {calc_proxy} \n"
+        content += f"LIMIT:{HRC_THRESHOLD['Warning']} counts/sec.\n"
+
+        subject = f"[sot_red_alert] Warning: HRC proxy"
+    
+    if calc_proxy > HRC_THRESHOLD['Violation']:
+        content = f"VIOLATION: A HRC GOES proxy violation has been observerd by GOES\n"
+        content += f"Observed: {calc_proxy} \n"
+        content += f"LIMIT:{HRC_THRESHOLD['Violation']} counts/sec.\n"
+
+        subject = f"[sot_red_alert] Violation: HRC proxy"
+    
+    if content != '':
+        send_mail(content, subject, HRC_ADMIN)
+        
 #----------------------------------------------------------------------------------
 #-- send_email_to_admin: send out a notification email to admin                  --
 #----------------------------------------------------------------------------------
@@ -55,6 +157,7 @@ def send_mail(content, subject, admin):
     input:  admin --- list of emails
     output: email to admin
     """
+    content += f"This message was send to {" ".join(admin)}"
     cmd = f'echo "{content}" | mailx -s "{subject}" {" ".join(admin)}'
     os.system(cmd)
 
