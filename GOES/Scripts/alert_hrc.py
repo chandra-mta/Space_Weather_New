2b967c7c813de0067f33dea318abe770ef26457f
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2024-03-22 15:30:05 -0400

    format alert check

--- GOES/Scripts/alert_hrc.py
+++ GOES/Scripts/alert_hrc.py
@@ -1,27 +1,26 @@
 #!/proj/sot/ska3/flight/bin/python
 import os
-import sys
 import argparse
 import getpass
 import traceback
-import subprocess
 import datetime
 import json
-import numpy as np
-from astropy.io import misc, ascii
-from astropy.table import Table, vstack, unique
+from astropy.io import ascii
 
 #
 #--- Define Directory Pathing
 #
 GOES_DIR = "/data/mta4/Space_Weather/GOES/Data"
 GOES_DATA_FILE = f"{GOES_DIR}/Gp_pchan_5m.txt"
-HRC_PROXY_DATA_FILE = f"{GOES_DIR}/hrc_proxy.h5"
 VIOL_RECORD_FILE = f"{GOES_DIR}/hrc_proxy_viol.json"
 
 NAMES = ('time', 'p1', 'p2a', 'p2b', 'p3', 'p4', 'p5',
          'p6', 'p7', 'p8a', 'p8b', 'p8c', 'p9', 'p10',
          'hrc_proxy', 'hrc_proxy_legacy')
+
+#Based on HRC Proxy differential values
+HRC_THRESHOLD = {'Warning': 6.2e4,
+                'Violation': 1.95e5}
 
 #Alert Email Addresses
 HRC_ADMIN = ['sot_ace_alert@cfa.harvard.edu']
@@ -30,14 +29,56 @@ HRC_ADMIN = ['sot_ace_alert@cfa.harvard.edu']
 def alert_hrc():
     dat = ascii.read(GOES_DATA_FILE, data_start=5, delimiter='\t', guess=False, names=NAMES)
     time, hrc_proxy, hrc_proxy_legacy = dat[-1]['time','hrc_proxy', 'hrc_proxy_legacy']
+    recent_data = {'time': time,
+                   'hrc_proxy': hrc_proxy,
+                   'hrc_proxy_legacy': hrc_proxy_legacy}
+
+    #Check current status of HRC proxy violations.
+    #If one has been found very recently, do not email about the violation again
+    with open(VIOL_RECORD_FILE) as f:
+        curr_viol = json.load(f)
+    
+    #Iterate over kinds of threshold and versions each proxy
+    content = ''
+    for kind in HRC_THRESHOLD.keys():
+        for proxy in ['hrc_proxy', 'hrc_proxy_legacy']:
+            if recent_data[proxy] > HRC_THRESHOLD[kind]:
+                #check if there was a similar kind of violation withing the last 24 hours
+                if viol_time_check(curr_viol, kind, proxy):
+                    content += f"{kind}: {proxy}\n"
+                    content += f"Observed: {recent_data[proxy]:.5e} at Time: {time}\n"
+                    content += f"Limit: {HRC_THRESHOLD[kind]:.3e} counts/sec.\n"
+                    content += f"{'-' * 20}\n"
+                    curr_viol[f"{kind}_{proxy}"] = recent_data
+    
+    if content != '':
+        send_mail(content, "HRC Proxy Violation", HRC_ADMIN)
+
+def send_mail(content, subject, admin):
+    """
+    send out a notification email to admin
+    """
+    content += f'This message was send to {" ".join(admin)}'
+    cmd = f'echo "{content}" | mailx -s "{subject}" {" ".join(admin)}'
+    os.system(cmd)
+
+def viol_time_check(curr_viol, kind, proxy):
+    """
+    Prevents spamming violaiton emails if the data is in violation, 
+    opting to send out a email if the specific violation was last warned more than 24 hours ago.
+    """
+    time_string = curr_viol[f"{kind}_{proxy}"]['time']
+    last = datetime.datetime.strptime(time_string, '%Y:%j:%H:%M')
+    now = datetime.datetime.utcnow()
+    return (now - last).seconds > 24 * 60 * 60
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-m", "--mode", choices = ['flight','test'], required = True, help = "Determine running mode.")
     parser.add_argument("-e", '--email', nargs = '*', required = False, help = "List of emails to recieve notifications")
     parser.add_argument("-g", "--goes", help = "Determine GOES + HRC data file path")
-    parser.add_argument("-p", "--hrc_proxy", help = "Determine realtime HRC proxy data file path")
-    parser.add_argument("-j", "--json", help = "Pass in custom data record for current state of HRC proxy violations.")
+    parser.add_argument("-j", "--json", help = "Pass in record for current state of HRC proxy violations.")
     args = parser.parse_args()
 
     if args.mode == 'test':
@@ -54,12 +95,7 @@ if __name__ == "__main__":
 #
         if args.goes:
             GOES_DATA_FILE = args.goes
-        if args.hrc_proxy:
-            HRC_PROXY_DATA_FILE = args.hrc_proxy
-        else:
-            OUT_DIR = f"{os.getcwd()}/test/outTest"
-            os.makedirs(OUT_DIR, exist_ok = True)
-            HRC_PROXY_DATA_FILE = f"{OUT_DIR}/hrc_proxy.h5"        
+
         if args.json:
             VIOL_RECORD_FILE = args.json
         else:
@@ -67,21 +103,18 @@ if __name__ == "__main__":
             #a typical script run more directly.
             OUT_DIR = f"{os.getcwd()}/test/outTest"
             os.makedirs(OUT_DIR, exist_ok = True)
-            temp_dict = {'in_viol': False,
-                         'last_viol_time': '2020:077:17:10:00',
-                         'content': '',
-                         'notified': True}
+            temp_dict = {"time": '2020:077:17:10',
+                         "hrc_proxy": 0,
+                         "hrc_proxy_legacy": 0}
             import copy
-            check_viol = {"warn_V1": copy.copy(temp_dict),
-                          "viol_V1": copy.copy(temp_dict),
-                          "warn_V2": copy.copy(temp_dict),
-                          "viol_V2": copy.copy(temp_dict)}
+            check_viol = {"Warning_hrc_proxy": copy.copy(temp_dict),
+                          "Violation_hrc_proxy": copy.copy(temp_dict),
+                          "Warning_hrc_proxy_legacy": copy.copy(temp_dict),
+                          "Violation_hrc_proxy_legacy": copy.copy(temp_dict)}
             
             VIOL_RECORD_FILE = f"{OUT_DIR}/hrc_proxy_viol.json"
             with open(VIOL_RECORD_FILE,'w') as f:
                 json.dump(check_viol, f, indent = 4)
-
-
         try:
             alert_hrc()
         except:
