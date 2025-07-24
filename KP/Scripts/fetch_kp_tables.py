630c25012c62b76508552e1f4ffb464580a02296
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2025-07-24 10:37:47 -0400

    Script header. Initial Commit.

---
+++ KP/Scripts/fetch_kp_tables.py
@@ -0,0 +1,9 @@
+#!/proj/sot/ska3/flight/bin/python
+"""
+
+**fetch_kp_tables.py**: Fetch KP index forecast tables and data from SWPC NOAA
+
+:Author: W. Aaron (william.aaron@cfa.harvard.edu)
+:Last Updated: Jul 24, 2025
+
+"""
