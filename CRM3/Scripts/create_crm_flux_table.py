175cf2b5fb519a4921cf3c819ec7e55862040e90
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2025-08-18 16:33:59 -0400

    Write the CRM fluence archive to the web directory as well.

--- CRM3/Scripts/create_crm_flux_table.py
+++ CRM3/Scripts/create_crm_flux_table.py
@@ -145,6 +145,7 @@ def archive(previous_table):
         attenuated_crm_fluence
     ])
     archive_table.write(f"{OUT_CRM_DATA_DIR}/CRMarchive.ecsv", overwrite=True, delimiter=',')
+    archive_table.write(f"{OUT_CRM_WEB_DIR}/CRMarchive.ecsv", overwrite=True, delimiter=',')
     previous_table.write(f"{OUT_CRM_DATA_DIR}/previous_crm_flux_table.ecsv", overwrite=True, delimiter=',')
 
 def reconnect(func):
