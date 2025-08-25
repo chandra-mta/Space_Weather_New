0d6f803c08e0fc0060b8e4f8091a72f38074d227
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2025-08-25 14:11:39 -0400

    Format syntax to ruff linting standards for comment line length reduction. Unique line comments are now formatted in Sphinx autodoc format.

--- CRM3/Scripts/create_crm_summary.py
+++ CRM3/Scripts/create_crm_summary.py
@@ -6,6 +6,7 @@
 :Last Updated: Aug 11, 2025
 
 """
+
 import os
 import json
 import numpy as np
@@ -28,19 +29,32 @@ GOES_DATA_DIR = "/data/mta4/Space_Weather/GOES/Data"
 #
 # --- Template Globals
 #
-_JINJA_ENV = Environment(loader = FileSystemLoader('Template', followlinks = True), keep_trailing_newline=True) #: Old format has an additional newline character
-_JINJA_ENV.filters['e_format'] = lambda v, p: f"{v:.{p}e}" #: Custom filter to format to scientific notation
-_JINJA_ENV.filters['date_format'] = lambda v: f"{v.split('.')[0]}" #: Custom filter to remove cxotime fractional seconds
+#: Keep trailing newline as old format has an additional newline character.
+_JINJA_ENV = Environment(
+    loader=FileSystemLoader("Template", followlinks=True), keep_trailing_newline=True
+)
+#: Custom filter to format to scientific notation.
+_JINJA_ENV.filters["e_format"] = lambda v, p: f"{v:.{p}e}"
+#: Custom filter to remove cxotime fractional seconds
+_JINJA_ENV.filters["date_format"] = lambda v: f"{v.split('.')[0]}"
 #
 # --- Globals
 #
-GOES_P4_RADMON_FACTOR = 3.4 #: This factor converts the GOES-R P4 channel flux (already recorded in MeV) into the RADMON P4GM units.
-GOES_P7_RADMON_FACTOR = 12 #: This factor converts the GOES-R P7 channel flux (already recorded in MeV) into the RADMON P41GM units
+#: This factor converts the GOES-R P4 channel flux (already recorded in MeV) into the RADMON P4GM units.
+GOES_P4_RADMON_FACTOR = 3.4
+#: This factor converts the GOES-R P7 channel flux (already recorded in MeV) into the RADMON P41GM units
+GOES_P7_RADMON_FACTOR = 12
 TDELTA = 300
 
 CXONOW = CxoTime()
-ISONOW = CXONOW.isot.split('.')[0] + "Z"
-SOL_REGION = ['NULL', 'Solar_Wind', 'Magnetosheath', 'Magnetosphere'] #: Indexed according to the CRM solar region index marker
+ISONOW = CXONOW.isot.split(".")[0] + "Z"
+SOL_REGION = [
+    "NULL",
+    "Solar_Wind",
+    "Magnetosheath",
+    "Magnetosphere",
+]  #: Indexed according to the CRM solar region index marker
+
 
 def create_crm_summary():
     """
@@ -48,20 +62,22 @@ def create_crm_summary():
     """
 
     summary, orbit_meta = read_crm_flux_table()
-    summary['sol_region'] = SOL_REGION[summary['sol_region_idx']] #: Additional clarity
-    summary['cxonow'] = CxoTime(summary['cxosecs']).date
+    #: Include solar region name for additional clarity.
+    summary["sol_region"] = SOL_REGION[summary["sol_region_idx"]]
+    summary["cxonow"] = CxoTime(summary["cxosecs"]).date
     supp_data = crm_supplementary_info()
     summary.update(supp_data)
     summary.update(orbit_meta)
     summary = _coerce_json_serialize(summary)
 
-    with open(f"{OUT_CRM_DATA_DIR}/CRMsummary.json", 'w') as f:
-        json.dump(summary, f, indent = 4)
+    with open(f"{OUT_CRM_DATA_DIR}/CRMsummary.json", "w") as f:
+        json.dump(summary, f, indent=4)
 
-    crm_template = _JINJA_ENV.get_template('CRMsummary.jinja')
-    crm_render = crm_template.render(data = summary)
-    with open(f"{OUT_CRM_DATA_DIR}/CRMsummary.dat", 'w') as fo:
+    crm_template = _JINJA_ENV.get_template("CRMsummary.jinja")
+    crm_render = crm_template.render(data=summary)
+    with open(f"{OUT_CRM_DATA_DIR}/CRMsummary.dat", "w") as fo:
         fo.write(crm_render)
+
 
 def read_crm_flux_table():
     """
@@ -69,16 +85,19 @@ def read_crm_flux_table():
     """
     crm_flux_table = ascii.read(f"{OUT_CRM_DATA_DIR}/crm_flux_table.ecsv")
 
-    corrected_crm_fluence = sum(crm_flux_table['corrected_crm_flux'] * TDELTA)
-    attenuated_crm_fluence = sum(crm_flux_table['attenuated_crm_flux'] * TDELTA)
+    corrected_crm_fluence = sum(crm_flux_table["corrected_crm_flux"] * TDELTA)
+    attenuated_crm_fluence = sum(crm_flux_table["attenuated_crm_flux"] * TDELTA)
 
     summary = {}
-    last_entry = {col: crm_flux_table[-1][col].tolist() for col in crm_flux_table.columns}
+    last_entry = {
+        col: crm_flux_table[-1][col].tolist() for col in crm_flux_table.columns
+    }
     summary.update(last_entry)
-    summary['corrected_crm_fluence'] = corrected_crm_fluence
-    summary['attenuated_crm_fluence'] = attenuated_crm_fluence
+    summary["corrected_crm_fluence"] = corrected_crm_fluence
+    summary["attenuated_crm_fluence"] = attenuated_crm_fluence
 
     return summary, crm_flux_table.meta
+
 
 def crm_supplementary_info():
     """
@@ -91,68 +110,76 @@ def crm_supplementary_info():
     ephem_data = read_ephem()
     supp_data.update(ephem_data)
     return supp_data
-    
+
+
 def read_goes():
     """
-    Read the most recent GOES flux data for P4, P7, and >= E2 
+    Read the most recent GOES flux data for P4, P7, and >= E2
     """
-    goes_data = {'goes_p4_flux': None,
-                 'goes_p7_flux': None,
-                 'goes_e2_flux': None,}
+    goes_data = {
+        "goes_p4_flux": None,
+        "goes_p7_flux": None,
+        "goes_e2_flux": None,
+    }
     diff_proton_table = ascii.read(f"{GOES_DATA_DIR}/goes_differential_protons.ecsv")
     intg_electron_table = ascii.read(f"{GOES_DATA_DIR}/goes_integral_electrons.ecsv")
 
-    #: In case the most recent flux for the target channel is missing, record the last known values by iterating backwards.
+    #: In case the most recent flux for the target channel is missing,
+    #: record the last known values by iterating backwards.
     idx = -1
-    while goes_data['goes_p4_flux'] is None:
-        a = diff_proton_table['P4'][idx]
+    while goes_data["goes_p4_flux"] is None:
+        a = diff_proton_table["P4"][idx]
         if a == np.ma.masked:
             idx -= 1
         else:
-            goes_data['goes_p4_flux'] = a * GOES_P4_RADMON_FACTOR
+            goes_data["goes_p4_flux"] = a * GOES_P4_RADMON_FACTOR
     idx = -1
-    while goes_data['goes_p7_flux'] is None:
-        b = diff_proton_table['P7'][idx]
+    while goes_data["goes_p7_flux"] is None:
+        b = diff_proton_table["P7"][idx]
         if b == np.ma.masked:
             idx -= 1
         else:
-            goes_data['goes_p7_flux'] = b * GOES_P7_RADMON_FACTOR
+            goes_data["goes_p7_flux"] = b * GOES_P7_RADMON_FACTOR
     idx = -1
-    while goes_data['goes_e2_flux'] is None:
-        c = intg_electron_table['>=2 MeV'][idx]
+    while goes_data["goes_e2_flux"] is None:
+        c = intg_electron_table[">=2 MeV"][idx]
         if c == np.ma.masked:
             idx -= 1
         else:
-            goes_data['goes_e2_flux'] = c
+            goes_data["goes_e2_flux"] = c
 
     return goes_data
+
 
 def read_ephem():
     """
     Read the EPHEM file to determine orbit altitude and leg
-    
+
     The EPHEM file records altitude in meters. This fetch returns in km.
     """
     with open(f"{EPHEM_DATA_DIR}/gephem.dat") as f:
-        data = f.read().split() #: Located on the first and only line.
+        data = f.read().split()  #: Located on the first and only line.
         alt = int(float(data[0]) / 1000)
         leg = data[1]
-    return {'orbit_altitude': alt, 'orbit_leg': leg}
+    return {"orbit_altitude": alt, "orbit_leg": leg}
+
 
 def _coerce_json_serialize(obj):
     def _coerce(x):
-        if x.__class__.__module__ == 'numpy':
+        if x.__class__.__module__ == "numpy":
             return x.tolist()
         elif isinstance(x, CxoTime):
             return x.date
         else:
             return x
-    if isinstance(obj,dict):
-        return {key:_coerce(obj[key]) for key in obj.keys()}
-    elif isinstance(obj,list):
+
+    if isinstance(obj, dict):
+        return {key: _coerce(obj[key]) for key in obj.keys()}
+    elif isinstance(obj, list):
         return [_coerce(i) for i in obj]
     else:
         return _coerce(obj)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -172,11 +199,11 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
     #
-    # --- Determine if running in test mode and change pathing if so
+    # --- Determine if running in test mode and change pathing if so.
     #
     if args.mode == "test":
         #
-        # --- Path output to same location as unit tests
+        # --- Path output to same location as unit tests.
         #
         OUT_CRM_DATA_DIR = f"{os.getcwd()}/test/_outTest"
         if args.path:
@@ -193,21 +220,29 @@ if __name__ == "__main__":
         if os.path.isfile(f"/tmp/{user}/{name}.lock"):
             with open(f"/tmp/{user}/{name}.lock") as f:
                 pid = int(f.readlines()[-1].strip())
-            #: Kill old process if stalling
+            #: Kill old process if stalling.
             try:
-                os.kill(pid,signal.SIGTERM)
+                os.kill(pid, signal.SIGTERM)
             except ProcessLookupError:
                 pass
-            os.system(f"mkdir -p /tmp/{user}; echo '{os.getpid()}' > /tmp/{user}/{name}.lock")
+            os.system(
+                f"mkdir -p /tmp/{user}; echo '{os.getpid()}' > /tmp/{user}/{name}.lock"
+            )
         else:
-            os.system(f"mkdir -p /tmp/{user}; echo '{os.getpid()}' > /tmp/{user}/{name}.lock")
+            os.system(
+                f"mkdir -p /tmp/{user}; echo '{os.getpid()}' > /tmp/{user}/{name}.lock"
+            )
 
         create_crm_summary()
-        #: Make available on the web
-        os.system(f"cp {OUT_CRM_DATA_DIR}/CRMsummary.json {OUT_CRM_WEB_DIR}/CRMsummary.json")
-        os.system(f"cp {OUT_CRM_DATA_DIR}/CRMsummary.dat {OUT_CRM_WEB_DIR}/CRMsummary.dat")
+        #: Make available on the web.
+        os.system(
+            f"cp {OUT_CRM_DATA_DIR}/CRMsummary.json {OUT_CRM_WEB_DIR}/CRMsummary.json"
+        )
+        os.system(
+            f"cp {OUT_CRM_DATA_DIR}/CRMsummary.dat {OUT_CRM_WEB_DIR}/CRMsummary.dat"
+        )
 
         #
-        # --- Remove lock file once process is completed
+        # --- Remove lock file once process is completed.
         #
         os.system(f"rm /tmp/{user}/{name}.lock")
