95f48e3ac649faf7eba93d7018b9d255dae4ffca
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2026-01-28 16:00:42 -0500

    refactor script to use existing goes products and update archive cleanly.

--- GOES/Scripts/collect_goes_long.py
+++ GOES/Scripts/collect_goes_long.py
@@ -9,35 +9,33 @@
 """
 import os
 import sys
-import time
 from cxotime import CxoTime
-import urllib.request
 import json
 import argparse
 import traceback
-
+from astropy.io import ascii
+import file_readers as fr
 #
 # --- Define directory pathing
 #
 GOES_DATA_DIR = "/data/mta4/Space_Weather/GOES/Data"
 OUT_DATA_DIR = "/data/mta4/Space_Weather/GOES/Data"
 
-PLINK = "https://services.swpc.noaa.gov/json/goes/primary/differential-protons-7-day.json"  #: json data locations proton and electron
-PROTON_LIST = [
-    "1020-1860 keV",
-    "1900-2300 keV",
-    "2310-3340 keV",
-    "3400-6480 keV",
-    "5840-11000 keV",
-    "11640-23270 keV",
-    "25900-38100 keV",
-    "40300-73400 keV",
-    "83700-98500 keV",
-    "99900-118000 keV",
-    "115000-143000 keV",
-    "160000-242000 keV",
-    "276000-404000 keV",
-]  #: proton energy designations and output file names
+_ARCHIVE_COLS = (
+    'P1',
+    'P2A',
+    'P2B',
+    'P3',
+    'P4',
+    'P5',
+    'P6',
+    'P7',
+    'P8A',
+    'P8B',
+    'P8C',
+    'P9',
+    'P10',
+)
 
 
 def collect_goes_long():
@@ -50,187 +48,61 @@ def collect_goes_long():
     #
     # --- find the last entry time
     #
-    outfile = f"{GOES_DATA_DIR}/goes_data_r.txt"
-    with open(outfile, "r") as f:
-        data = [line.strip() for line in f.readlines()]
-        data = [line for line in data if line != ""]
-    cut = 0
-    while cut == 0:
-        atemp = data[-1].split()
-        try:
-            cut = CxoTime(atemp[0]).secs
-            break
-        except:  # noqa: E722
-            data.pop(-1)
+    last_line = fr.get_last_text_line(f"{GOES_DATA_DIR}/goes_data_r.txt")
+    cutoff = CxoTime(last_line.split()[0])
     #
     # --- extract proton data
     #
-    p_data = extract_goes_data(PLINK, PROTON_LIST)
+    goes_table = ascii.read(f"{GOES_DATA_DIR}/goes_differential_protons.ecsv")
+    cxotime = CxoTime(goes_table['time_tag'].data)
+    goes_table.add_column(cxotime, name = 'cxotime')
     #
-    # --- time list
+    # --- Select new data lines
     #
-    t_list = p_data[0][0]
-    d_len = len(t_list)
+    sel = goes_table['cxotime'] > cutoff
+    subtable = goes_table[sel]
     #
     # --- compute hrc proxy
     #
-    hrc_val = compute_hrc(p_data)
+    hrc_proxy = []
+    for row in subtable:
+        _ = compute_hrc(row)
+        hrc_proxy.append(_)
+    subtable.add_column(hrc_proxy, name = "HRC_Proxy")
     #
-    # --- aline will save the text output of the table which is used by CRM
+    # --- format archive addition
     #
-    line = ""
-
-    for k in range(0, d_len):
-        stime = CxoTime(t_list[k]).secs
-        if stime <= cut:
-            #
-            # ---If the time is less or equal to this cutoff point, then it's not new data.
-            #
-            continue
-        line += f"{t_list[k]}\t\t"
-
-        for m in range(0, 13):
-            try:
-                line += f"{p_data[m][1][k]:1.3e}\t"
-            except:  # noqa: E722
-                line += "0.0\t"
-
-        line += f"{hrc_val[k]:5.0f}\t\n"
-    #
-    # ---  print out data file for ACIS Rad use
-    #
-    appendout = f"{OUT_DATA_DIR}/{os.path.basename(outfile)}"
-    with open(appendout, "a") as fo:
-        fo.write(line)
+    line =''
+    for row in subtable:
+        line += format_archive_line(row)
+    
+    with open(f"{OUT_DATA_DIR}/goes_data_r.txt", 'a') as f:
+        f.write(line)
 
 
-def extract_goes_data(dlink, energy_list):
-    """Extract GOES satellite flux data
-
-    :param dlink: JSON web address
-    :type dlink: str
-    :param energy_list: A list of energy designation
-    :type energy_list: list
-    :return: a list of lists, the first tier of lists consist of each energy band, and the second tier of lists mark time points and corresponding fluxes
-    :rtype: list(list)
+def compute_hrc(row):
     """
-    @rerun
-    def _goes_swpc_fetch():
-        """
-        Internal function for wrapping the internet data fetch in a rerun decorator in case of download errors.
-        """
-        with urllib.request.urlopen(dlink, timeout=10) as url:
-            data = json.loads(url.read().decode())  #: Read json file from the web
-        return data
-    data = _goes_swpc_fetch()
-    #
-    # --- go through all energy ranges
-    #
-    elen = len(energy_list)
-    d_save = []
-    for k in range(0, elen):
-        t_list = []
-        f_list = []
-        energy = energy_list[k]
-        last_time = time.strptime(data[0]["time_tag"], "%Y-%m-%dT%H:%M:%SZ")
-        for ent in data:
-            #
-            # --- read time and flux of the given energy range
-            #
-            if ent["energy"] == energy:
-                flux = float(ent["flux"]) * 1e3  # --- keV to MeV
-                #
-                # --- convert time into seconds from 1998.1.1
-                #
-                otime = ent["time_tag"]
-                otime = time.strptime(otime, "%Y-%m-%dT%H:%M:%SZ")
-
-                #
-                # ---If the otime is more than five minutes after the last_time,
-                # ---then that means the data set is missing an entry for this energy band and zero values should be append.
-                #
-                diff = time.mktime(otime) - time.mktime(last_time)
-                if diff > 300:
-                    for i in range(
-                        300, int(diff), 300
-                    ):  #: All times should be in divisions of 5 minutes/300 seconds.
-                        missing_time = time.localtime(time.mktime(last_time) + i)
-                        t_list.append(time.strftime("%Y:%j:%H:%M:%S", missing_time))
-                        f_list.append(
-                            -1e5
-                        )  #: Mark missing data with the invalid data marker (-1e5)
-
-                t_list.append(
-                    time.strftime("%Y:%j:%H:%M:%S", otime)
-                )  #: record time as string
-                f_list.append(flux)
-                last_time = otime
-
-        d_save.append([t_list, f_list])
-
-    return d_save
-
-
-def compute_hrc(data):
-    """Compute hrc proxy value.
-    HRC_PROXY = 6000 x P4 + 270000 x P5 + 100000 x P6
-        P4 ~ 11640-23270 keV + 25900-38100 keV
-        P5 ~ 40300-73400 keV,
-        P6 ~ 83700-98500 keV + 99900-118000 keV + 115000-143000 keV + 160000-242000 keV.
-        and:
-        c0: '1020-1860 keV',
-        c1: '1900-2300 keV',
-        c2: '2310-3340 keV',
-        c3: '3400-6480 keV',
-        c4: '5840-11000 keV',
-        c5: '11640-23270 keV',
-        c6: '25900-38100 keV',
-        c7: '40300-73400 keV',
-        c8: '83700-98500 keV',
-        c9: '99900-118000 keV',
-        c10: '115000-143000 keV',
-        c11: '160000-242000 keV',
-        c12: '276000-404000 keV'
-
-    :param data: a list of lists of data: [[<time>, <data1>], [<time>, <data2>],...]
-    :type data: list
-    :return: hrc proxy list
-    :rtype: list
+    :NOTE: The HRC Proxy was calculated based on GOES channel flux rates in MeV units.
+        The MTA GOES data sets use MeV units accoridngly, but documentation of GOES uses KeV.
+        This proxy equation for the GOES-R series was put in use as of 2021:125:06:05:00.
+    
+    P5 = 11.64 - 23.27 MeV
+    P6 = 25.90 - 38.10 MeV
+    P7 = 40.30 - 73.40 MeV
     """
-    c5 = data[5][1]
-    c6 = data[6][1]
-    c7 = data[7][1]
+    
+    hrc_proxy = (143 * row['P5']) + (64738 * row['P6']) + (162505 * row['P7']) + 4127
+    return hrc_proxy
 
-    hrc = []
-    for k in range(0, len(c5)):
-        try:
-            val = (
-                143.0 * c5[k] + 64738.0 * c6[k] + 162505.0 * c7[k] + 4127
-            )  #: New proxy as of 2021:125:06:05:00
-        except:  # noqa: E722
-            val = 0.0
-
-        hrc.append(val)
-
-    return hrc
-
-def rerun(func):
+def format_archive_line(row):
     """
-    Function decorator which sleeps and reruns the provided function upon encountering a set of errors.
+    Format the astropy table into the goes_data_r.txt specific archive format.
     """
-    _freq = 3
-    _errors = (json.decoder.JSONDecodeError, urllib.error.URLError)
-    def wrapper_func(*args,**kwargs):
-        _last_exception = None
-        for i in range(_freq):
-            try:
-                return func(*args, **kwargs)
-            except _errors as e:
-                _last_exception = e
-                time.sleep(5)
-        _last_exception.add_note(f'Decorator ran function {_freq} times. Still encountered error.')
-        raise _last_exception
-    return wrapper_func
+    line = f"{row['cxotime'].date.split('.')[0]}\t\t"
+    line += "\t".join([f"{i:1.3e}" for i in row[_ARCHIVE_COLS]])
+    line += f"\t{row['HRC_Proxy']:5.0f}\t\n"
+    
+    return line
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -252,8 +124,8 @@ if __name__ == "__main__":
     if args.mode == "test":
         OUT_DATA_DIR = f"{os.getcwd()}/test/_outTest"
         os.makedirs(OUT_DATA_DIR, exist_ok=True)
-        if os.path.isfile(f"{OUT_DATA_DIR}/goes_data_r.txt"):
-            GOES_DATA_DIR = OUT_DATA_DIR
+        #if os.path.isfile(f"{OUT_DATA_DIR}/goes_data_r.txt"):
+        #    GOES_DATA_DIR = OUT_DATA_DIR
         collect_goes_long()
     else:
         #
