fcba6e7a6409d5d1e54466e8f39e7e6c5c5626a6
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2026-03-09 13:37:10 -0400

    Shellscript refactor.

---
+++ GOES/Scripts/goes.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+cd ${SPACE_WEATHER}/GOES/Scripts
+
+python fetch_goes_tables.py -m flight
+python plot_goes_data.py -m flight
+python update_goes_html_page.py -m flight
+python alert_hrc.py -m flight
