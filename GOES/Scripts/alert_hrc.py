fb6bdccb5ece47a9a60af374ff9c98c67abbfb82
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2026-03-10 09:47:12 -0400

    Refactor lock file handling for stalls and errors to use process managment libraries.

--- GOES/Scripts/alert_hrc.py
+++ GOES/Scripts/alert_hrc.py
@@ -14,6 +14,7 @@
 
 from email.mime.text import MIMEText
 import os
+import shutil
 import signal
 import argparse
 from subprocess import PIPE, Popen
@@ -22,6 +23,7 @@ import json
 import csv
 from astropy.io import ascii
 from pathlib import Path
+import psutil
 #
 # --- Define Directory Pathing
 #
@@ -200,32 +202,27 @@ if __name__ == "__main__":
         alert_hrc()
 
     elif args.mode == "flight":
-        #
-        # --- Create a lock file and exit strategy in case of race conditions
-        #
+        TESTMAIL = False
+        #: Create a lock file and exit strategy in case of stall.
         name = os.path.basename(__file__).split(".")[0]
-        user = getpass.getuser()
-        if os.path.isfile(f"/tmp/{user}/{name}.lock"):
-            notification = f"Lock file exists as /tmp/{user}/{name}.lock. Process already running/errored out. " 
-            notification += "Check calling scripts/cronjob/cronlog. Killing old process."
-            #: Email alert if the script stalls out, since HRC alerting depends on output
-            send_mail(notification, f"Stalled Script: {name}", ADMIN)
-            with open(f"/tmp/{user}/{name}.lock") as f:
-                pid = int(f.readlines()[-1].strip())
-            #: Kill old stalling process and remove corresponding lock file.
-            os.remove(f"/tmp/{user}/{name}.lock")
-            os.kill(pid,signal.SIGTERM)
-            #: Generate lock file for the current corresponding process
-            os.system(f"mkdir -p /tmp/{user}; echo '{os.getpid()}' > /tmp/{user}/{name}.lock")
-        else:
-            #: Previous script run must have completed successfully. Prepare lock file for this script run.
-            os.system(f"mkdir -p /tmp/{user}; echo '{os.getpid()}' > /tmp/{user}/{name}.lock")
+        user = os.getenv("USER", "mta")
+        lock = Path("/tmp", user, f"{name}.lock")
 
-        try:
-            alert_hrc()
-        except:  # noqa: E722
-            traceback.print_exc()
-        #
-        # --- Remove lock file once process is completed
-        #
-        os.system(f"rm /tmp/{user}/{name}.lock")
+        #: If lock file exists, read the pid and kill the process, then remove the lock file
+        if os.path.isfile(lock):
+            notification = f"Lock file exists as {lock} Process already running/errored out. Check calling scripts/cronjob/cronlog. Killing old process." 
+            send_mail(f"Stalled Script: {name}", notification, ADMIN)
+            with open(lock) as f:
+                pid = int(f.read().strip())
+            if psutil.pid_exists(pid):
+                os.kill(pid, signal.SIGTERM)
+            os.remove(lock)
+        
+        #: Lock file with current pid
+        pid = os.getpid()
+        os.makedirs(lock.parent, exist_ok = True)
+        with open(lock, 'w') as f:
+            f.write(str(pid))
+        alert_hrc()
+        #: Remove lock file once process is completed
+        os.remove(lock)
