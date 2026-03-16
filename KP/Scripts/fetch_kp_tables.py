a38123f3734dcdd6e0ae86a687a64655e658be23
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2026-03-16 13:05:42 -0400

    Refactor all file paths into cross-platform pathlib.Path() instances.

--- KP/Scripts/fetch_kp_tables.py
+++ KP/Scripts/fetch_kp_tables.py
@@ -31,10 +31,12 @@ import getpass
 import signal
 import numpy as np
 import file_readers as fr
+from pathlib import Path
 #
 # --- Define Directory Pathing
 #
-KP_DATA_DIR = "/data/mta4/Space_Weather/KP/Data"
+SPACE_WEATHER = Path(os.getenv('SPACE_WEATHER', "/data/mta4/Space_Weather"))
+KP_DATA_DIR : Path = SPACE_WEATHER / "KP" / "Data"
 #
 # --- Globals
 #
@@ -68,26 +70,26 @@ def fetch_kp_tables():
     swpc_kp = fetch_SWPC_KP()
     iaga_kp = fetch_IAGA_KP()
 
-    swpc_filename = f"{KP_DATA_DIR}/kp_swpc.ecsv"
+    swpc_filename = KP_DATA_DIR / "kp_swpc.ecsv"
     swpc_kp.meta['description'] = "Forecast of the planetary KP index as sourced from the SWPC. Includes observed, estimated, and predicted values. https://www.swpc.noaa.gov/products/planetary-k-index."
     swpc_kp.meta['sources'] = [
         {'origin_link': SOURCE_SWPC,
          'origin_script': os.path.abspath(__file__),
          'update_time': CXONOW.date,
          'mta_owned_origin': False,
-         'output_file': swpc_filename
+         'output_file': str(swpc_filename)
         }
     ]
     swpc_kp.write(swpc_filename, overwrite=True, delimiter=',')
 
-    iaga_filename = f"{KP_DATA_DIR}/kp_iaga.ecsv"
+    iaga_filename = KP_DATA_DIR / "kp_iaga.ecsv"
     iaga_kp.meta['description'] = "Observations of the planetary KP index as compiled by the IAGA. https://www-app3.gfz-potsdam.de/kp_index/qlyymm.html."
     iaga_kp.meta['sources'] = [
         {'origin_link': SOURCE_IAGA,
          'origin_script': os.path.abspath(__file__),
          'update_time': CXONOW.date,
          'mta_owned_origin': False,
-         'output_file': iaga_filename
+         'output_file': str(iaga_filename)
         }
     ]
     iaga_kp.write(iaga_filename, overwrite=True, delimiter=',')
@@ -210,7 +212,7 @@ def write_legacy_files(swpc_kp):
         return line
     
     #: Only write up to the current time block, either observed or estimated.
-    past_archive = f"{KP_DATA_DIR}/k_index_data_past"
+    past_archive = KP_DATA_DIR / "k_index_data_past"
     past_archive_line = fr.get_last_text_line(past_archive)
     start = CxoTime(int(past_archive_line.split('\t')[0]))
     stop = CxoTime()
@@ -224,18 +226,18 @@ def write_legacy_files(swpc_kp):
     
     with open(past_archive,'a') as f:
         f.write(append_past_archive)
-    with open(f"{KP_DATA_DIR}/solar_wind_data_past.txt",'a') as f:
+    with open(KP_DATA_DIR / "solar_wind_data_past.txt", 'a') as f:
         f.write(append_past_solar)
     
     #: Of the most recent data, write the most recent entry if one exists to update
     if len(swpc_kp[sel]) > 0:
         current_entry = swpc_kp[sel][-1]
         line = _format_sol(current_entry)
-        with open(f"{KP_DATA_DIR}/kp.dat",'w') as f:
+        with open(KP_DATA_DIR / "kp.dat",'w') as f:
             f.write(HEADER+line)
     
     #: Now write the forecast archive.
-    forecast_archive = f"{KP_DATA_DIR}/k_index_data"
+    forecast_archive = KP_DATA_DIR / "k_index_data"
     forecast_archive_line = fr.get_last_text_line(forecast_archive)
     start = CxoTime(int(past_archive_line.split('\t')[0]))
     sel = start <= CxoTime(swpc_kp['time_tag'].data)
@@ -248,7 +250,7 @@ def write_legacy_files(swpc_kp):
     
     with open(forecast_archive,'a') as f:
         f.write(append_forecast_archive)
-    with open(f"{KP_DATA_DIR}/solar_wind_data.txt",'a') as f:
+    with open(KP_DATA_DIR / "solar_wind_data.txt", 'a') as f:
         f.write(append_forecast_solar)
 
 if __name__ == "__main__":
@@ -259,11 +261,11 @@ if __name__ == "__main__":
 
     if args.mode == 'test':
         if args.path:
-            KP_DATA_DIR = args.path
+            KP_DATA_DIR = Path(args.path)
         else:
-            KP_DATA_DIR = f"{os.getcwd()}/test/_outTest"
+            KP_DATA_DIR = Path(os.getcwd(), "test", "_outTest")
         os.makedirs(KP_DATA_DIR, exist_ok=True)
-
+        
         fetch_kp_tables()
 
     elif args.mode == "flight":
