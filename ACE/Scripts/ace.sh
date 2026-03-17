5d4d9569a39deb8a8ad3282f6b04c64a87fe41ef
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2026-03-17 16:15:32 -0400

    Refactor the shell-changing wrapper scripts to a uniform environment portable bash script.

---
+++ ACE/Scripts/ace.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+cd ${SPACE_WEATHER}/ACE/Scripts
+
+python update_ace_data_files.py -m flight
+python plot_p3_data.py -m flight
+python create_ace_html_page.py -m flight
+python compute_fluence_cxo70.py -m flight
